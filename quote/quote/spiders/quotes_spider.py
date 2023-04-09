be7fe64a294fbce864c4f586ba1ba277cf327e36
Devesh <d.mishra56603@gmail.com>
2023-04-09 22:33:42 +0530

    updated script

--- quote/quote/spiders/quotes_spider.py
+++ quote/quote/spiders/quotes_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy.spiders import Spider
 
+
 class QuoteSpyder(Spider):
     name = 'quotes'
     start_urls = [
@@ -10,4 +11,13 @@ class QuoteSpyder(Spider):
     def parse(self, response):
         # title = response.css('title::text').extract()
         # yield {'titletext': title}
+        all_div_quotes = response.css('div.quote')[0]
+        title = all_div_quotes.css('span.text::text').extract()
+        author = all_div_quotes.css('.author::text').extract()
+        tag = all_div_quotes.css('.tags::text').extract()
+        yield {
+            'title': title,
+            'author': author,
+            'tag': tag
+        }
         
