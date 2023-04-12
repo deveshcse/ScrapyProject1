a47ca79b07fabb61c220e7b9b5633e15c94ac7f2
Devesh <d.mishra56603@gmail.com>
2023-04-13 01:21:25 +0530

    modified code to scrape one quote, author and tags one by one

--- quote/quote/spiders/quotes_spider.py
+++ quote/quote/spiders/quotes_spider.py
@@ -12,12 +12,13 @@ class QuoteSpyder(Spider):
         # title = response.css('title::text').extract()
         # yield {'titletext': title}
         all_div_quotes = response.css('div.quote')
-        title = all_div_quotes.css('span.text::text').extract()
-        author = all_div_quotes.css('.author::text').extract()
-        tag = all_div_quotes.css('.tag::text').extract()
-        yield {
-            'title': title,
-            'author': author,
-            'tag': tag
-        }
+        for quotes in all_div_quotes:
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tag = quotes.css('.tag::text').extract()
+            yield {
+                'title': title,
+                'author': author,
+                'tag': tag
+            }
         
