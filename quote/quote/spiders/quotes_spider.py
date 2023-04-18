76d0d4f4e7ca7a5284f24314eba01fa49a3e5fb4
Devesh <d.mishra56603@gmail.com>
2023-04-19 01:17:14 +0530

    scrapping pages navigating through next pages

--- quote/quote/spiders/quotes_spider.py
+++ quote/quote/spiders/quotes_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapy.spiders import Spider
+from ..items import QuoteItem
 
 
 class QuoteSpyder(Spider):
@@ -9,16 +10,23 @@ class QuoteSpyder(Spider):
     ]
 
     def parse(self, response):
-        # title = response.css('title::text').extract()
-        # yield {'titletext': title}
+        items = QuoteItem()
+
         all_div_quotes = response.css('div.quote')
         for quotes in all_div_quotes:
             title = quotes.css('span.text::text').extract()
             author = quotes.css('.author::text').extract()
             tag = quotes.css('.tag::text').extract()
-            yield {
-                'title': title,
-                'author': author,
-                'tag': tag
-            }
+
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
+        next_page = response.css('li.next a::attr(href)').get()
+        print(next_page)
+        if next_page is not None:
+
+            yield response.follow(next_page, callback=self.parse)
+
         
